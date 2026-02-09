a442463532f1783db799b467ad5cabed9199a099
alumno <V0034icky@hotmail.com>
2026-02-09 08:51:59 -0700

    [AGREGA] Añade proyecto de xcode.

---
+++ prueba_hola_mundo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  prueba_hola_mundo
+//
+//  Created by alumno on 2/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world uwu!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ prueba_hola_mundo/prueba_hola_mundoApp.swift
@@ -0,0 +1,17 @@
+//
+//  prueba_hola_mundoApp.swift
+//  prueba_hola_mundo
+//
+//  Created by alumno on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct prueba_hola_mundoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
